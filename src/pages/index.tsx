e102d51f364fb9f29acdb69cf8f1d473a2beb546
Lauren <laurenmiles.dev@gmail.com>
2023-05-23 11:35:27 -0400

    Updated index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,21 @@
+import Navigation from '@/components/navigation/Navigation';
 import Introduction from '@/components/introduction/Introduction';
+
+import Tab from '@/types/tab';
 
 export default function Home() {
   const introProps = {
     title: "Hello, I'm Lauren.",
     description: 'Software developer with a passion for creating modern web pages',
   };
+  const pages: Tab[] = [
+    new Tab(
+      'Home',
+      <Introduction title={introProps.title} description={introProps.description}></Introduction>
+    ),
+  ];
 
-  return (
-    <Introduction title={introProps.title} description={introProps.description}></Introduction>
-  );
+  return <Navigation pages={pages}></Navigation>;
 }
 
 export function getStaticProps() {
