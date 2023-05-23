b2f532b1e7c592bc0f8a3c2c8933f1a52c1a8a5b
Lauren <laurenmiles.dev@gmail.com>
2023-05-23 10:38:29 -0400

    Created Introduction module and added to index page

---
+++ src/components/introduction/Introduction.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import styles from './Introduction.module.css';
+import img from '../../../public/img/IMG_8341.jpg';
+
+type Props = {
+  title: string;
+  description: string;
+};
+
+export default function Introduction(props: Props) {
+  return (
+    <div className={`${styles.container} col-md-12`}>
+      <div className={`${styles.left} col-md-7`}>
+        <div className={`${styles.title} col-md-12`}>{props.title}</div>
+        <div className={`${styles['description-container']} col-md-12`}>
+          <div className={styles.description}>{props.description}</div>
+        </div>
+      </div>
+      <div className={`${styles.right} col-md-5`}>
+        <Image src={img} alt={'Image of Lauren'} className={styles.photo} />
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,20 @@
+import Introduction from '@/components/introduction/Introduction';
+
 export default function Home() {
-  return <></>;
+  const introProps = {
+    title: "Hello, I'm Lauren.",
+    description: 'Software developer with a passion for creating modern web pages',
+  };
+
+  return (
+    <Introduction title={introProps.title} description={introProps.description}></Introduction>
+  );
 }
 
 export function getStaticProps() {
   return {
     props: {
-      title: 'Title',
+      title: 'Lauren Miles',
     },
   };
 }
