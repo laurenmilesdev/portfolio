8fdebdfb7fb6307d9c3fc86735326ae132abc7e8
Lauren <laurenmiles.dev@gmail.com>
2023-06-01 12:48:11 -0400

    Updated index view based on component changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,28 @@
 import Data from '../../data.json';
 import Navigation from '@/components/navigation/Navigation';
+import Layout from '@/components/layout/Layout';
 import Introduction from '@/components/introduction/Introduction';
 import About from '@/components/about/About';
 
-import Tab from '@/types/tab';
+import TabType from '@/types/tab';
+import ImageType from '@/types/image';
+
+import img from '../../public/img/IMG_8341.jpg';
 
 const introductionComponent = (): JSX.Element => (
-  <Introduction
-    title={Data.introduction.title}
-    description={Data.introduction.subtitle}
-  ></Introduction>
+  <Introduction subtitle={Data.introduction.subtitle}></Introduction>
 );
 
-const aboutComponent = (): JSX.Element => (
-  <About title={Data.about.title} description={Data.about.description}></About>
-);
+const aboutComponent = (): JSX.Element => <About description={Data.about.description}></About>;
 
 export default function Home(): JSX.Element {
-  const pages: Tab[] = [
-    new Tab('Home', introductionComponent()),
-    new Tab('About', aboutComponent()),
-  ];
+  const intro = getContent(
+    Data.introduction.title,
+    introductionComponent(),
+    new ImageType(img, 'Image of Lauren')
+  );
+  const about = getContent(Data.about.title, aboutComponent());
+  const pages: TabType[] = [new TabType('Home', intro), new TabType('About', about)];
 
   return <Navigation pages={pages}></Navigation>;
 }
@@ -31,4 +33,8 @@ export function getStaticProps() {
       title: 'Lauren Miles',
     },
   };
+}
+
+export function getContent(title: string, component: JSX.Element, image?: ImageType) {
+  return <Layout title={title} content={component} image={image} />;
 }
