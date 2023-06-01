64c74c4b7b6670cff603d688a30080a99c0091e4
Lauren <laurenmiles.dev@gmail.com>
2023-06-01 13:51:21 -0400

    Added tab transition

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Fade from '@mui/material/Fade';
 import styles from './Layout.module.css';
 import ImageType from '@/types/image';
 
@@ -11,20 +12,22 @@ type Props = {
 
 export default function Layout(props: Props): JSX.Element {
   return (
-    <div className={`${styles.container} col-md-12`}>
-      <div className={styles.left}>
-        <div
-          className={`${props.animatedTitle ? styles['title-animated'] : styles.title} col-md-12`}
-        >
-          {props.title}
+    <Fade in={true}>
+      <div className={`${styles.container} col-md-12`}>
+        <div className={styles.left}>
+          <div
+            className={`${props.animatedTitle ? styles['title-animated'] : styles.title} col-md-12`}
+          >
+            {props.title}
+          </div>
+          <div className={`${styles['description-container']} col-md-12`}>{props.content}</div>
         </div>
-        <div className={`${styles['description-container']} col-md-12`}>{props.content}</div>
+        <div className={styles.right}>
+          {props.image && (
+            <Image src={props.image.src} alt={props.image.altText} className={styles.photo} />
+          )}
+        </div>
       </div>
-      <div className={styles.right}>
-        {props.image && (
-          <Image src={props.image.src} alt={props.image.altText} className={styles.photo} />
-        )}
-      </div>
-    </div>
+    </Fade>
   );
 }
