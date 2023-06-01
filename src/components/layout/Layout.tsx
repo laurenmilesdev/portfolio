325776a546991053cc0d838f8ac272895d790159
Lauren <laurenmiles.dev@gmail.com>
2023-06-01 17:24:10 -0400

    Updated layout and index

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,13 +1,17 @@
 import Image from 'next/image';
 import Fade from '@mui/material/Fade';
-import styles from './Layout.module.css';
+
+import Email from '../contact/Email';
 import ImageType from '@/types/image';
+
+import styles from './Layout.module.css';
 
 type Props = {
   title: string;
   content: JSX.Element;
   animatedTitle: boolean;
   image?: ImageType;
+  email?: boolean;
 };
 
 export default function Layout(props: Props): JSX.Element {
@@ -15,16 +19,18 @@ export default function Layout(props: Props): JSX.Element {
     <Fade in={true}>
       <div className={`${styles.container} col-md-12`}>
         <div className={styles.left}>
-          <div
-            className={`${props.animatedTitle ? styles['title-animated'] : styles.title} col-md-12`}
-          >
-            {props.title}
+          <div className={`${props.animatedTitle ? styles['title-animated'] : ''} col-md-12`}>
+            <h1>{props.title}</h1>
           </div>
           <div className={`${styles['description-container']} col-md-12`}>{props.content}</div>
         </div>
         <div className={styles.right}>
-          {props.image && (
+          {props.image ? (
             <Image src={props.image.src} alt={props.image.altText} className={styles.photo} />
+          ) : props.email ? (
+            <Email />
+          ) : (
+            <></>
           )}
         </div>
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Navigation from '@/components/navigation/Navigation';
 import Layout from '@/components/layout/Layout';
 import Introduction from '@/components/introduction/Introduction';
 import About from '@/components/about/About';
-import Contact from '@/components/contact/Contact';
+import Contacts from '@/components/contact/Contacts';
 
 import introImg from '../../public/img/introduction/IMG_8341.jpg';
 import aboutImg from '../../public/img/about/IMG_1465.jpg';
@@ -12,13 +12,13 @@ import TabType from '@/types/tab';
 import ImageType from '@/types/image';
 
 const introductionComponent = (): JSX.Element => (
-  <Introduction subtitle={Data.introduction.subtitle}></Introduction>
+  <Introduction subtitle={Data.introduction.subtitle} />
 );
 
-const aboutComponent = (): JSX.Element => <About description={Data.about.description}></About>;
+const aboutComponent = (): JSX.Element => <About description={Data.about.description} />;
 
 const contactComponent = (): JSX.Element => (
-  <Contact description={Data.contact.description} contacts={Data.contact.contacts}></Contact>
+  <Contacts description={Data.contact.description} contacts={Data.contact.contacts} />
 );
 
 export default function Home(): JSX.Element {
@@ -31,14 +31,20 @@ export default function Home(): JSX.Element {
     introductionImage
   );
   const about = getContent(Data.about.title, aboutComponent(), true, aboutImage);
-  const contact = getContent(Data.contact.title as string, contactComponent(), true);
+  const contact = getContent(
+    Data.contact.title as string,
+    contactComponent(),
+    true,
+    undefined,
+    true
+  );
   const pages: TabType[] = [
     new TabType('Home', intro),
     new TabType('About', about),
     new TabType('Contact', contact),
   ];
 
-  return <Navigation pages={pages}></Navigation>;
+  return <Navigation pages={pages} />;
 }
 
 export function getStaticProps() {
@@ -53,7 +59,16 @@ export function getContent(
   title: string,
   component: JSX.Element,
   animatedTitle: boolean,
-  image?: ImageType
+  image?: ImageType,
+  email?: boolean
 ) {
-  return <Layout title={title} content={component} animatedTitle={animatedTitle} image={image} />;
+  return (
+    <Layout
+      title={title}
+      content={component}
+      animatedTitle={animatedTitle}
+      image={image}
+      email={email}
+    />
+  );
 }
