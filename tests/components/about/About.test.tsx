2cafbe56ca43d51b4277a87ea9980b19bcab0212
Lauren <laurenmiles.dev@gmail.com>
2023-06-06 12:57:50 -0400

    Updated About.tsx

--- tests/components/about/About.test.tsx
+++ tests/components/about/About.test.tsx
@@ -3,11 +3,12 @@ import About from '../../../src/components/about/About';
 
 describe('About component', () => {
   it('renders string[] of paragraphs', () => {
+    const dataTestId = 'about-paragraph';
     const paragraphs = ['This description paragraph 1', 'This is description paragraph 2'];
 
     const { getByTestId } = render(<About paragraphs={paragraphs} />);
 
-    expect(getByTestId('paragraph-0')).toHaveTextContent(paragraphs[0]);
-    expect(getByTestId('paragraph-1')).toHaveTextContent(paragraphs[1]);
+    expect(getByTestId(`${dataTestId}-0`)).toHaveTextContent(paragraphs[0]);
+    expect(getByTestId(`${dataTestId}-1`)).toHaveTextContent(paragraphs[1]);
   });
 });
