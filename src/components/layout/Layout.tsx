7afff6ce21eae505ac5bb0dd136b00d44f536496
Lauren <laurenmiles.dev@gmail.com>
2023-06-06 13:00:54 -0400

    Cleaned up Layout.tsx and interior components

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,31 +2,25 @@ import Fade from '@mui/material/Fade';
 import LeftContainer from './LeftContainer';
 import RightContainer from './RightContainer';
 
-import ImageType from '@/types/component-helpers/image';
-
 import styles from './Layout.module.css';
 
 type Props = {
   title: string;
-  content: JSX.Element;
   animatedTitle: boolean;
-  includeEmail: boolean;
-  emailSubtitle: string;
-  emailDescription: string;
-  image?: ImageType;
+  leftComponent: JSX.Element;
+  rightComponent?: JSX.Element;
 };
 
 export default function Layout(props: Props): JSX.Element {
   return (
     <Fade in={true}>
-      <div className={`${styles.container} col-md-12`}>
-        <LeftContainer title={props.title} content={props.content} animated={props.animatedTitle} />
-        <RightContainer
-          includeEmail={props.includeEmail}
-          emailSubtitle={props.emailSubtitle}
-          emailDescription={props.emailDescription}
-          image={props.image}
+      <div className={styles.container}>
+        <LeftContainer
+          title={props.title}
+          content={props.leftComponent}
+          animated={props.animatedTitle}
         />
+        <RightContainer content={props.rightComponent} />
       </div>
     </Fade>
   );
--- src/components/layout/RightContainer.tsx
+++ src/components/layout/RightContainer.tsx
@@ -1,26 +1,13 @@
-import Image from 'next/image';
-import Email from '../contact/Email';
-import ImageType from '@/types/component-helpers/image';
-
 import styles from './RightContainer.module.css';
 
 type Props = {
-  includeEmail: boolean;
-  emailSubtitle: string;
-  emailDescription: string;
-  image?: ImageType;
+  content?: JSX.Element;
 };
 
 export default function RightContainer(props: Props): JSX.Element {
   return (
     <div className={styles['right-container']} data-testid="right-container">
-      {props.includeEmail ? (
-        <Email subtitle={props.emailSubtitle} description={props.emailDescription} />
-      ) : props.image ? (
-        <Image src={props.image.src} alt={props.image.altText} className={styles.photo} />
-      ) : (
-        <></>
-      )}
+      {props.content ?? <></>}
     </div>
   );
 }
