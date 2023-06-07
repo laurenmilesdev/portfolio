f613c4801c332690cc85c34666e448cc7e4bc143
Lauren <laurenmiles.dev@gmail.com>
2023-06-07 16:45:53 -0400

    Updated Project.tsx

--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -1,4 +1,5 @@
-import Link from 'next/link';
+import { Button } from '@mui/material';
+import Fade from '@mui/material/Fade';
 import TabPanel from '../tabs/TabPanel';
 import Technologies from './Technologies';
 import ProjectType from '@/types/projects/project';
@@ -10,28 +11,28 @@ type Props = {
 };
 
 const content = (project: ProjectType, index: number): JSX.Element => (
-  <div>
-    <h2 data-testid={`project-title-${index}`}>{project.title}</h2>
+  <>
+    <h2 data-testid={`project-title-${index}`}>{project.title.toUpperCase()}</h2>
     {project.technologies && <Technologies technologies={project.technologies} />}
     <div data-testid={`project-description-${index}`}>
       <p>{project.description}</p>
     </div>
     <p data-testid={`project-company-${index}`}>
-      <Link href={project.companyUrl ?? ''} target="_blank">
+      <Button className="btn-primary" href={project.companyUrl ?? ''} target="_blank">
         {project.company}
-      </Link>
+      </Button>
     </p>
-  </div>
+  </>
 );
 
 export default function Project(props: Props): JSX.Element {
   return (
     <div className="col-md-12">
-      <p data-testid="description">{props.pageDescription}</p>
-      <hr />
       {props.projects.map((project: ProjectType, index: number) => (
         <TabPanel value={props.value} index={index} key={index}>
-          {content(project, index)}
+          <Fade in={true}>
+            <div className="m-4"> {content(project, index)}</div>
+          </Fade>
         </TabPanel>
       ))}
     </div>
