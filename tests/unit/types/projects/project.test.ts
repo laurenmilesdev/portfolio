784a83d94faa82bd556c56c67dbd4d3108f4ec57
Lauren <laurenmiles.dev@gmail.com>
2023-06-06 21:24:44 -0400

    Updated project type

--- tests/unit/types/projects/project.test.ts
+++ tests/unit/types/projects/project.test.ts
@@ -1,17 +1,32 @@
 import Project from '../../../../src/types/projects/project';
 
 describe('Project', () => {
-  it('returns Project type object', () => {
-    const title = 'title';
-    const description = 'description';
-    const company = 'company';
-    const technologies = ['tech 1', 'tech 2'];
-    const response = new Project(title, description, company, technologies);
+  const title = 'title';
+  const description = 'description';
+  const company = 'company';
+  const technologies = ['tech 1', 'tech 2'];
+  const companyUrl = 'company.com';
+
+  it('returns Project type object without company url', () => {
+    const response = new Project(title, company, technologies, description);
     const expectedResponse: Project = {
       title,
       description,
       company,
       technologies,
+    };
+
+    expect(response).toEqual(expectedResponse);
+  });
+
+  it('returns Project type object with company url', () => {
+    const response = new Project(title, company, technologies, description, companyUrl);
+    const expectedResponse: Project = {
+      title,
+      description,
+      company,
+      technologies,
+      companyUrl,
     };
 
     expect(response).toEqual(expectedResponse);
