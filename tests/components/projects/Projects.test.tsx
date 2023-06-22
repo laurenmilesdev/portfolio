f694437aef933378b8c886e52b007bd7837ed5f8
Lauren <laurenmiles.dev@gmail.com>
2023-06-22 14:22:42 -0400

    Updated Project and Projects components

--- tests/components/projects/Projects.test.tsx
+++ tests/components/projects/Projects.test.tsx
@@ -3,23 +3,17 @@ import Projects from '../../../src/components/projects/Projects';
 import ProjectType from '../../../src/types/projects/project';
 
 describe('Projects component', () => {
-  const description = 'This is a description';
   const technologies = ['tech 1', 'tech 2'];
   const projects: ProjectType[] = [
     new ProjectType('Project 1', 'Company 1', technologies, 'Project 1 description'),
     new ProjectType('Project 2', 'Company 2', technologies, 'Project 2 description', 'url.com'),
   ];
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => console.log('test');
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => console.log(newValue);
   const value = 1;
 
   it('renders tabs', () => {
     const { getByTestId } = render(
-      <Projects
-        description={description}
-        projects={projects}
-        handleChange={handleChange}
-        value={value}
-      />
+      <Projects projects={projects} handleChange={handleChange} value={value} />
     );
 
     expect(getByTestId('projects-tabs')).toHaveTextContent(projects[0].title);
