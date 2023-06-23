8b543a6a07f54f187cc6fb2ed222de0717a844cb
Lauren <laurenmiles.dev@gmail.com>
2023-06-23 13:39:43 -0400

    Moved bulk of app to _app from index

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,54 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useState } from 'react';
 import { Container } from '@mui/material';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/globals.css';
 
+import Data from '../../data.json';
+import Layout from '../components/layout/Layout';
+import Navigation from '../components/navigation/Navigation';
+import Introduction from '../components/introduction/Introduction';
+import About from '../components/about/About';
+import Contact from '../components/contact/Contact';
+import Projects from '../components/projects/Projects';
+import Contacts from '../components/contact/Contacts';
+import TabType from '../types/component-helpers/tab';
+
 export default function App({ Component, pageProps }: AppProps) {
+  const [pageValue, setPageValue] = useState<number>(0);
+  const [projectValue, setProjectValue] = useState<number>(0);
+
+  const introductionComponent = () => <Introduction subtitle={Data.home.subtitle} />;
+  const aboutComponent = () => <About description={Data.about.description} />;
+  const portfolioComponent = () => (
+    <Projects
+      projects={Data.portfolio.projects}
+      handleChange={handleProjectChange}
+      value={projectValue}
+    />
+  );
+  const contactComponent = () => <Contact description={Data.contact.description} />;
+
+  const introPage = <Layout component={introductionComponent()} />;
+  const aboutPage = <Layout component={aboutComponent()} title={Data.about.title} />;
+  const portfolioPage = <Layout component={portfolioComponent()} title={Data.portfolio.title} />;
+  const contactPage = <Layout component={contactComponent()} title={Data.contact.title} />;
+  const pages: TabType[] = [
+    new TabType(Data.home.title as string, introPage),
+    new TabType(Data.about.title, aboutPage),
+    new TabType(Data.portfolio.title as string, portfolioPage),
+    new TabType(Data.contact.title, contactPage),
+  ];
+
+  function handlePageChange(event: React.SyntheticEvent, newValue: number) {
+    setPageValue(newValue);
+  }
+
+  function handleProjectChange(event: React.SyntheticEvent, newValue: number) {
+    setProjectValue(newValue);
+  }
+
   return (
     <>
       <Head>
@@ -14,9 +58,18 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Navigation
+        pageLabels={pages.map((page) => page.label)}
+        handleChange={handlePageChange}
+        value={pageValue}
+        // useDarkTheme={useDarkTheme} handleThemeChange={handleThemeChange}
+      />
+
       <Container>
-        <Component {...pageProps} />
+        <Component {...pageProps} pageValue={pageValue} pages={pages} />
       </Container>
+
+      <Contacts contacts={Data.contact.contacts} />
     </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,14 @@
-import { useEffect, useState } from 'react';
-import Data from '../../data.json';
+import { useEffect } from 'react';
 // import { getTheme } from '@/helpers/helpers';
-import Navigation from '@/components/navigation/Navigation';
-import Layout from '@/components/layout/Layout';
-import Introduction from '@/components/introduction/Introduction';
-import About from '@/components/about/About';
-import Contacts from '@/components/contact/Contacts';
-import Projects from '@/components/projects/Projects';
+import TabPanel from '@/components/tabs/TabPanel';
 import TabType from '@/types/component-helpers/tab';
 
-export default function Home(): JSX.Element {
-  const [projectValue, setProjectValue] = useState<number>(0);
-  // const [useDarkTheme, setUseDarkTheme] = useState<boolean>(true);
-  const introductionComponent = () => <Introduction subtitle={Data.introduction.subtitle} />;
-  const aboutComponent = () => <About description={Data.about.description} />;
-  const projectsComponent = () => (
-    <Projects projects={Data.project.projects} handleChange={handleChange} value={projectValue} />
-  );
-  const contactComponent = () => (
-    <Contacts description={Data.contact.description} contacts={Data.contact.contacts} />
-  );
+type Props = {
+  pageValue: number;
+  pages: TabType[];
+};
 
-  const introPage = pageContent(introductionComponent());
-  const aboutPage = pageContent(aboutComponent(), Data.about.title);
-  const projectsPage = pageContent(projectsComponent(), Data.project.title);
-  const contactPage = pageContent(contactComponent(), Data.contact.title);
-  const pages: TabType[] = [
-    new TabType('Home', introPage),
-    new TabType('About', aboutPage),
-    new TabType('Projects', projectsPage),
-    new TabType('Contact', contactPage),
-  ];
-
-  function handleChange(event: React.SyntheticEvent, newValue: number) {
-    setProjectValue(newValue);
-  }
-
+export default function Home({ pageValue, pages }: Props): JSX.Element {
   // function handleThemeChange(event: React.SyntheticEvent, newUseDarkTheme: boolean) {
   //   const theme: string = getTheme(newUseDarkTheme);
 
@@ -49,15 +22,14 @@ export default function Home(): JSX.Element {
   });
 
   return (
-    <Navigation
-      pages={pages}
-      // useDarkTheme={useDarkTheme} handleThemeChange={handleThemeChange}
-    />
+    <>
+      {pages.map(({ component }, index: number) => (
+        <TabPanel value={pageValue} index={index} key={index}>
+          {component}
+        </TabPanel>
+      ))}
+    </>
   );
-}
-
-function pageContent(component: JSX.Element, title?: string) {
-  return <Layout component={component} title={title} />;
 }
 
 export function getStaticProps() {
