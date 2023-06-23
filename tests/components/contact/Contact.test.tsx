f594d266ee53293ddb245ee5ca1e2fc5ffa789b2
Lauren <laurenmiles.dev@gmail.com>
2023-06-23 15:39:49 -0400

    Fixed test

--- tests/components/contact/Contact.test.tsx
+++ tests/components/contact/Contact.test.tsx
@@ -1,11 +1,16 @@
 import { render } from '@testing-library/react';
 import Contact from '../../../src/components/contact/Contact';
+import ContactType from '../../../src/types/contact/contact';
 
 describe('Contact component', () => {
   const description = 'This is a description';
+  const contacts = [
+    new ContactType('GitHub', 'Contact description.', 'Contact value'),
+    new ContactType('LinkedIn', 'Contact description 2.', 'Contact value 2', 'url.com'),
+  ];
 
   it('renders page description', () => {
-    const { getByTestId } = render(<Contact description={description} />);
+    const { getByTestId } = render(<Contact description={description} contacts={contacts} />);
 
     expect(getByTestId('description')).toHaveTextContent(description);
   });
