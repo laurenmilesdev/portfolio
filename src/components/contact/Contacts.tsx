4ab034251e05d7e8b29b2aa770927811297d3e7b
Lauren <laurenmiles.dev@gmail.com>
2023-06-23 11:36:08 -0400

    Updated Contacts component

--- src/components/contact/Contacts.tsx
+++ src/components/contact/Contacts.tsx
@@ -9,9 +9,12 @@ type Props = {
 export default function Contacts(props: Props): JSX.Element {
   return (
     <div className="col-md-12">
-      <div className="col-md-12 mb-3" data-testid="description">
-        {props.description}
-      </div>
+      <div
+        className="col-md-12 mb-3"
+        data-testid="description"
+        dangerouslySetInnerHTML={{ __html: props.description }}
+      />
+
       <div className="col-md-12">
         <ul>
           {props.description &&
