e6c82bd35f15a1f0fcdea3e7941f46b51cb14360
Lauren <laurenmiles.dev@gmail.com>
2023-06-25 10:44:24 -0400

    Updated Contacts component and tests

--- tests/components/contact/Contacts.test.tsx
+++ tests/components/contact/Contacts.test.tsx
@@ -3,15 +3,29 @@ import Contacts from '../../../src/components/contact/Contacts';
 import ContactType from '../../../src/types/contact/contact';
 
 describe('Contacts component', () => {
+  let getByTestId: any;
   const contacts = [
-    new ContactType('GitHub', 'Contact description.', 'Contact value'),
-    new ContactType('LinkedIn', 'Contact description 2.', 'Contact value 2', 'url.com'),
+    new ContactType('GitHub', 'Contact description.', 'Contact value', 'github.com'),
+    new ContactType('LinkedIn', 'Contact description 2.', 'Contact value 2', 'linkedin.com'),
+    new ContactType('Resume', 'Contact description 3.', 'Contact value 3'),
+    new ContactType('None', 'Contact description 4.', 'Contact value 4'),
   ];
 
-  it('renders contact buttons', () => {
-    const { getByTestId } = render(<Contacts contacts={contacts} />);
+  beforeEach(() => {
+    ({ getByTestId } = render(<Contacts contacts={contacts} />));
+  });
 
+  it('renders contact buttons with correct icons', () => {
     expect(getByTestId('GitHubIcon')).toBeVisible();
     expect(getByTestId('LinkedInIcon')).toBeVisible();
+    expect(getByTestId('PictureAsPdfIcon')).toBeVisible();
+  });
+
+  it('renders contact buttons with correct URLs', () => {
+    contacts.forEach((contact: ContactType, index: number) => {
+      const element = document.getElementById(`btn-${index}`);
+
+      expect(element).toHaveAttribute('href', contact.url);
+    });
   });
 });
