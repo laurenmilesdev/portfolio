a829746e12aea10b04683a8e3c4e9f8f28ec4043
Lauren <laurenmiles.dev@gmail.com>
2023-06-25 10:34:52 -0400

    Updated Contact component and tests

--- tests/components/contact/Contact.test.tsx
+++ tests/components/contact/Contact.test.tsx
@@ -1,17 +1,16 @@
 import { render } from '@testing-library/react';
 import Contact from '../../../src/components/contact/Contact';
-import ContactType from '../../../src/types/contact/contact';
 
 describe('Contact component', () => {
   const description = 'This is a description';
-  const contacts = [
-    new ContactType('GitHub', 'Contact description.', 'Contact value'),
-    new ContactType('LinkedIn', 'Contact description 2.', 'Contact value 2', 'url.com'),
-  ];
 
-  it('renders page description', () => {
-    const { getByTestId } = render(<Contact description={description} contacts={contacts} />);
+  beforeEach(() => {
+    render(<Contact description={description} />);
+  });
 
-    expect(getByTestId('description')).toHaveTextContent(description);
+  it('renders description', () => {
+    const element = document.getElementById('contact-description');
+
+    expect(element).toHaveTextContent(description);
   });
 });
