2ee30ffb37503787dd68c41bd767da84cc45dcc3
Lauren <laurenmiles.dev@gmail.com>
2023-06-25 10:52:55 -0400

    Added data mock for reusable testing data

--- tests/components/contact/Contacts.test.tsx
+++ tests/components/contact/Contacts.test.tsx
@@ -2,14 +2,10 @@ import { render } from '@testing-library/react';
 import Contacts from '../../../src/components/contact/Contacts';
 import ContactType from '../../../src/types/contact/contact';
 
+import { contacts } from '../../mocks/data-mock';
+
 describe('Contacts component', () => {
   let getByTestId: any;
-  const contacts = [
-    new ContactType('GitHub', 'Contact description.', 'Contact value', 'github.com'),
-    new ContactType('LinkedIn', 'Contact description 2.', 'Contact value 2', 'linkedin.com'),
-    new ContactType('Resume', 'Contact description 3.', 'Contact value 3'),
-    new ContactType('None', 'Contact description 4.', 'Contact value 4'),
-  ];
 
   beforeEach(() => {
     ({ getByTestId } = render(<Contacts contacts={contacts} />));
