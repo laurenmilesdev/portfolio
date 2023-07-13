f0710994e8ff918371ed098306ab6dcec61e3ba1
Lauren <laurenmiles.dev@gmail.com>
2023-07-13 12:12:44 -0400

    Added unit test for StartButton

--- src/components/windows-theme/start-bar/start-button/StartButton.tsx
+++ src/components/windows-theme/start-bar/start-button/StartButton.tsx
@@ -3,5 +3,7 @@ import { Button } from '@mui/material';
 import styles from './StartButton.module.css';
 
 export default function StartButton() {
-  return <Button className={`${styles['start-btn']} windows-box-shadow`}></Button>;
+  return (
+    <Button className={`${styles['start-btn']} windows-box-shadow`} id="windows-start-btn"></Button>
+  );
 }
