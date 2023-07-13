d93f4db61ead14cbc7b0791b048e1d0428774e7a
Lauren <laurenmiles.dev@gmail.com>
2023-07-13 17:27:03 -0400

    Added openCloseMenu function to button

--- src/components/windows-theme/start-bar/start-button/StartButton.tsx
+++ src/components/windows-theme/start-bar/start-button/StartButton.tsx
@@ -1,9 +1,14 @@
 import { Button } from '@mui/material';
+import { openCloseMenu } from '../../../../utils/window';
 
 import styles from './StartButton.module.css';
 
 export default function StartButton(): JSX.Element {
   return (
-    <Button className={`${styles['start-btn']} windows-box-shadow`} id="windows-start-btn"></Button>
+    <Button
+      className={`${styles['start-btn']} windows-box-shadow`}
+      id="windows-start-btn"
+      onClick={() => openCloseMenu()}
+    ></Button>
   );
 }
