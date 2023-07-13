fc13c13779f5357c893b866d19c15ee43cb1d0b6
Lauren <laurenmiles.dev@gmail.com>
2023-07-13 10:42:05 -0400

    Added StartButton component

---
+++ src/components/windows-theme/start-bar/start-button/StartButton.tsx
@@ -0,0 +1,7 @@
+import { Button } from '@mui/material';
+
+import styles from './StartButton.module.css';
+
+export default function StartButton() {
+  return <Button className={`${styles['start-btn']} windows-box-shadow`}></Button>;
+}
