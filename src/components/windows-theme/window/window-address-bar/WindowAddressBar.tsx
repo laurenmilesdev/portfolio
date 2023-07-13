d313bf7ef27c8bce5c8c2c7483a2f289844ff9f9
Lauren <laurenmiles.dev@gmail.com>
2023-07-13 11:15:50 -0400

    Added WindowsAddressBar component

---
+++ src/components/windows-theme/window/window-address-bar/WindowAddressBar.tsx
@@ -0,0 +1,15 @@
+import styles from './WindowAddressBar.module.css';
+
+export default function WindowAddressBar() {
+  return (
+    <div className={`${styles.options} ${styles.padding}`}>
+      <div className={styles.item}>Address</div>
+      <input
+        type="text"
+        className={styles['inverse-windows-box-shadow']}
+        value="https://portfolio.laurenmilesdev.com/"
+        readOnly
+      />
+    </div>
+  );
+}
