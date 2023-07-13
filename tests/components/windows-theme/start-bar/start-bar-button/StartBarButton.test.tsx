52158a464a0cbc668a984b063a5d6e573bf87df1
Lauren <laurenmiles.dev@gmail.com>
2023-07-13 12:12:57 -0400

    Added unit tests for StartBarButton

---
+++ tests/components/windows-theme/start-bar/start-bar-button/StartBarButton.test.tsx
@@ -0,0 +1,31 @@
+import { render } from '@testing-library/react';
+import StartBarButton from '../../../../../src/components/windows-theme/start-bar/start-bar-button/StartBarButton';
+import PageModel from '../../../../../src/models/component-helpers/page';
+
+describe('StartBarButton component', () => {
+  const pages = [
+    new PageModel('Page', <>Page</>),
+    new PageModel('Page 2', <>Page</>, 'window-id', 'button-id'),
+  ];
+
+  it('renders null if index is 0', () => {
+    const page = pages[0];
+
+    render(<StartBarButton page={page} index={0} />);
+
+    const element = document.getElementById(page.buttonId ?? '') as HTMLButtonElement;
+
+    expect(element).toEqual(null);
+  });
+
+  it('renders button if index is not 0', () => {
+    const page = pages[1];
+
+    render(<StartBarButton page={page} index={1} />);
+
+    const element = document.getElementById(page.buttonId ?? '') as HTMLButtonElement;
+
+    expect(element).toBeInTheDocument();
+    expect(element).toHaveTextContent(page.label);
+  });
+});
