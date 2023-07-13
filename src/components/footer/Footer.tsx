f70390728e4a955372bcdd284cea620017a9ee73
Lauren <laurenmiles.dev@gmail.com>
2023-07-13 17:13:34 -0400

    Updated Footer for new button

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,18 +1,39 @@
+import { Dispatch, SetStateAction, useState } from 'react';
+import WindowsStartBar from '../windows-theme/start-bar/StartBar';
+import WindowsThemeButton from '../windows-theme/windows-theme-button/WindowsThemeButton';
 import Contacts from '../contacts/Contacts';
 import ThemeSwitch from '../theme-switch/ThemeSwitch';
 import ContactModel from '../../models/contact/contact';
+import PageModel from '../../models/component-helpers/page';
 
 import styles from './Footer.module.css';
 
 type Props = {
+  pages: PageModel[];
   contacts: ContactModel[];
+  useWindowsTheme: boolean;
+  setUseWindowsTheme: Dispatch<SetStateAction<boolean>>;
 };
 
-export default function Footer({ contacts }: Props): JSX.Element {
-  return (
+export default function Footer({
+  pages,
+  contacts,
+  useWindowsTheme,
+  setUseWindowsTheme,
+}: Props): JSX.Element {
+  const [useDarkTheme, setUseDarkTheme] = useState<boolean>(true);
+
+  return useWindowsTheme ? (
+    <WindowsStartBar pages={pages} />
+  ) : (
     <div className={`${styles.container} col-md-12`}>
+      <WindowsThemeButton
+        useWindowsTheme={useWindowsTheme}
+        setUseWindowsTheme={setUseWindowsTheme}
+        useDarkTheme={useDarkTheme}
+      />
       <Contacts contacts={contacts} />
-      <ThemeSwitch />
+      <ThemeSwitch useDarkTheme={useDarkTheme} setUseDarkTheme={setUseDarkTheme} />
     </div>
   );
 }
