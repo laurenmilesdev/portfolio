dc3ca55414797bc700c05b537ff90f061587fc28
Lauren <laurenmiles.dev@gmail.com>
2023-08-30 10:29:26 -0400

    Updated Footer test for new functionality

--- tests/components/footer/Footer.test.tsx
+++ tests/components/footer/Footer.test.tsx
@@ -1,23 +1,83 @@
+import React, { Dispatch } from 'react';
 import { render } from '@testing-library/react';
 import Footer from '../../../src/components/footer/Footer';
-import { contacts } from '../../mocks/data-mock';
+import { contacts, pages } from '../../mocks/data-mock';
+import { useStateMock, setStateMock } from '../../mocks/use-state-mock';
+
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useState: jest.fn(),
+}));
+
+jest.mock('next/image', () => ({
+  __esModule: true,
+  // eslint-disable-next-line @next/next/no-img-element
+  default: (props: any) => <img {...props} />,
+}));
 
 describe('Footer component', () => {
   let getByTestId: any;
+  const setUseWindowsTheme = setStateMock;
 
-  beforeEach(() => {
-    ({ getByTestId } = render(<Footer contacts={contacts} />));
+  describe('Windows Theme disabled', () => {
+    const useWindowsTheme = false;
+
+    beforeEach(() => {
+      jest
+        .spyOn(React, 'useState')
+        .mockImplementation(useStateMock as () => [unknown, Dispatch<unknown>]);
+
+      ({ getByTestId } = render(
+        <Footer
+          pages={pages}
+          contacts={contacts}
+          useWindowsTheme={useWindowsTheme}
+          setUseWindowsTheme={setUseWindowsTheme}
+        />
+      ));
+    });
+
+    it('renders Contacts', () => {
+      expect(getByTestId('GitHubIcon')).toBeVisible();
+      expect(getByTestId('LinkedInIcon')).toBeVisible();
+      expect(getByTestId('PictureAsPdfIcon')).toBeVisible();
+    });
+
+    it('renders ThemeSwitch', () => {
+      const element = document.getElementById('theme-switch') as HTMLSpanElement;
+
+      expect(element).toBeInTheDocument();
+    });
+
+    it('renders Windows Theme button', () => {
+      const element = document.getElementById('windows-theme-btn') as HTMLButtonElement;
+
+      expect(element).toBeInTheDocument();
+    });
   });
 
-  it('renders Contacts', () => {
-    expect(getByTestId('GitHubIcon')).toBeVisible();
-    expect(getByTestId('LinkedInIcon')).toBeVisible();
-    expect(getByTestId('PictureAsPdfIcon')).toBeVisible();
-  });
+  describe('Windows Theme enabled', () => {
+    const useWindowsTheme = true;
 
-  it('renders ThemeSwitch', () => {
-    const element = document.getElementById('theme-switch') as HTMLSpanElement;
+    beforeEach(() => {
+      jest
+        .spyOn(React, 'useState')
+        .mockImplementation(useStateMock as () => [unknown, Dispatch<unknown>]);
 
-    expect(element).toBeInTheDocument();
+      ({ getByTestId } = render(
+        <Footer
+          pages={pages}
+          contacts={contacts}
+          useWindowsTheme={useWindowsTheme}
+          setUseWindowsTheme={setUseWindowsTheme}
+        />
+      ));
+    });
+
+    it('renders StartButton', () => {
+      const element = document.getElementById('windows-start-btn') as HTMLButtonElement;
+
+      expect(element).toBeInTheDocument();
+    });
   });
 });
