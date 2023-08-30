0a3f4f7867b5fa3520a55efa0e8889de40898828
Lauren <laurenmiles.dev@gmail.com>
2023-08-30 15:56:59 -0400

    Created Clock component

---
+++ src/components/windows-theme/start-bar/clock/Clock.tsx
@@ -0,0 +1,20 @@
+import styles from './Clock.module.css';
+
+export default function Clock() {
+  return <div className={`${styles.clock} col-md-12`}>{getCurrentTimeString(new Date())}</div>;
+}
+
+export function getCurrentTimeString(currentDate: Date) {
+  const minutes = currentDate.getMinutes();
+  let hours = currentDate.getHours();
+  let minutesString = '';
+  let ampm = 'PM';
+
+  if (hours > 12) hours -= 12;
+  else ampm = 'AM';
+
+  if (minutes < 10) minutesString = `0${minutes}`;
+  else minutesString = `${minutes}`;
+
+  return `${hours}:${minutesString} ${ampm}`;
+}
