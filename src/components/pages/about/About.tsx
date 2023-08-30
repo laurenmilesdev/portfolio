e29367a366936b7d5448de36ffba7931100257b0
Lauren <laurenmiles.dev@gmail.com>
2023-08-30 16:26:44 -0400

    Fixed small issue

--- src/components/pages/about/About.tsx
+++ src/components/pages/about/About.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image';
 import img from '../../../../public/img/about/IMG_1465.jpg';
 import styles from './About.module.css';
 
-export const descriptionDivId = 'about-description';
-export const imageId = 'about-img';
-export const altText = 'Image of Lauren as a child washing a truck with dad';
-
 type Props = {
   description: string;
 };
+
+export const descriptionDivId = 'about-description';
+export const imageId = 'about-img';
+export const altText = 'Image of Lauren as a child washing a truck with dad';
 
 export default function About({ description }: Props): JSX.Element {
   return (
--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -3,15 +3,15 @@ import Image from 'next/image';
 import img from '../../../../public/img/home/headshot.png';
 import styles from './Home.module.css';
 
-export const subtitleDivId = 'home-subtitle';
-export const descriptionDivId = 'home-description';
-export const imageId = 'logo';
-export const altText = 'Image of Lauren within text that says Lauren Miles';
-
 type Props = {
   subtitle: string;
   description: string;
 };
+
+export const subtitleDivId = 'home-subtitle';
+export const descriptionDivId = 'home-description';
+export const imageId = 'logo';
+export const altText = 'Image of Lauren within text that says Lauren Miles';
 
 export default function Home({ subtitle, description }: Props): JSX.Element {
   return (
