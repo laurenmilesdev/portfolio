7813eb1334f76adb3bfcbf2802c2d535fd306142
Lauren <laurenmiles.dev@gmail.com>
2023-08-30 09:43:29 -0400

    Updated contact data and model

--- src/models/contact/contact.ts
+++ src/models/contact/contact.ts
@@ -7,10 +7,23 @@ export default class Contact {
 
   url?: string;
 
-  constructor(name: string, description: string, value: string, url?: string) {
+  imgSrc?: string;
+
+  imgDescription?: string;
+
+  constructor(
+    name: string,
+    description: string,
+    value: string,
+    url?: string,
+    imgSrc?: string,
+    imgDescription?: string
+  ) {
     this.name = name;
     this.description = description;
     this.value = value;
     this.url = url;
+    this.imgSrc = imgSrc;
+    this.imgDescription = imgDescription;
   }
 }
