9ab918e3b2aa58eb6ba08bb108ada099c1ec08ba
Lauren <laurenmiles.dev@gmail.com>
2023-09-01 11:43:21 -0400

    Updated Navigation based on Windows theme

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -6,6 +6,7 @@ type Props = {
   pageValue: number;
   pageTitles: string[];
   handleChange: (event: React.SyntheticEvent, newValue: number) => void;
+  useWindowsTheme: boolean;
 };
 
 const a11yProps = (index: number): object => ({
@@ -13,20 +14,31 @@ const a11yProps = (index: number): object => ({
   'aria-controls': `tabpanel-${index}`,
 });
 
-export default function Navigation({ pageValue, pageTitles, handleChange }: Props): JSX.Element {
-  return (
+export default function Navigation({
+  pageValue,
+  pageTitles,
+  handleChange,
+  useWindowsTheme,
+}: Props): JSX.Element {
+  const tabs = (
+    <Tabs value={pageValue} onChange={handleChange} centered>
+      {pageTitles.map((title: string, index: number) => (
+        <Tab
+          className={`${styles.tab} nav-tab`}
+          label={title}
+          key={index}
+          {...a11yProps(index)}
+          id={`tab-${index}`}
+        />
+      ))}
+    </Tabs>
+  );
+
+  return useWindowsTheme ? (
+    <div className={`${styles['windows-theme-app-bar']} col-md-12`}>{tabs}</div>
+  ) : (
     <AppBar className={`${styles['app-bar']} col-md-12`} elevation={0}>
-      <Tabs value={pageValue} onChange={handleChange} centered>
-        {pageTitles.map((title: string, index: number) => (
-          <Tab
-            className={`${styles.tab} nav-tab`}
-            label={title}
-            key={index}
-            {...a11yProps(index)}
-            id={`tab-${index}`}
-          />
-        ))}
-      </Tabs>
+      {tabs}
     </AppBar>
   );
 }
