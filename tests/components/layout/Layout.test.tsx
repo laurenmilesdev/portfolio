a5f4d2820c16a223afabc5a719290271fb431fae
Lauren <laurenmiles.dev@gmail.com>
2023-09-01 10:45:09 -0400

    Fixed unit test

--- tests/components/layout/Layout.test.tsx
+++ tests/components/layout/Layout.test.tsx
@@ -2,26 +2,45 @@ import { render } from '@testing-library/react';
 import Layout, { titleDivId, contentDivId } from '../../../src/components/layout/Layout';
 
 describe('Layout component', () => {
-  const pageValue = 1;
-  const pageNavLabels = ['Title 1', 'Title 2'];
-  const pageTitles = ['', 'Title2'];
+  const pageTitles = ['Title', 'Title2'];
   const componentText = 'This is the component';
   const component = <div>{componentText}</div>;
 
-  beforeEach(() => {
-    render(
-      <Layout
-        pageValue={pageValue}
-        pageNavLabels={pageNavLabels}
-        pageTitles={pageTitles}
-        handlePageChange={() => undefined}
-      >
-        {component}
-      </Layout>
-    );
+  describe('title is undefined', () => {
+    const pageValue = 0;
+
+    beforeEach(() => {
+      render(
+        <Layout pageValue={pageValue} pageTitles={pageTitles} handlePageChange={() => undefined}>
+          {component}
+        </Layout>
+      );
+    });
+
+    it('does not render title', () => {
+      const element = document.getElementById(titleDivId) as HTMLDivElement;
+
+      expect(element).toBeNull();
+    });
+
+    it('renders content', () => {
+      const element = document.getElementById(contentDivId) as HTMLDivElement;
+
+      expect(element).toHaveTextContent(componentText);
+    });
   });
 
-  describe('title is not empty string', () => {
+  describe('title is not undefined', () => {
+    const pageValue = 1;
+
+    beforeEach(() => {
+      render(
+        <Layout pageValue={pageValue} pageTitles={pageTitles} handlePageChange={() => undefined}>
+          {component}
+        </Layout>
+      );
+    });
+
     it('renders title', () => {
       const element = document.getElementById(titleDivId) as HTMLDivElement;
 
