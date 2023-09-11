79fc1283f72d051026dfc351d320c11e7b887098
Lauren <laurenmiles.dev@gmail.com>
2023-09-11 11:28:31 -0400

    Test to see if order matters

--- src/components/windows-theme/start-bar/start-bar-button/StartBarButton.tsx
+++ src/components/windows-theme/start-bar/start-bar-button/StartBarButton.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image';
+import styles from './StartBarButton.module.css';
+// eslint-disable-next-line import/order
 import { Button } from '@mui/material';
 import WindowModel from '../../../../models/component-helpers/window';
 import { minimizeMaximizeWindow } from '../../../../utils/window';
 
 import img from '../../../../../public/img/windows/ie.png';
-import styles from './StartBarButton.module.css';
 
 type Props = { window: WindowModel };
 
