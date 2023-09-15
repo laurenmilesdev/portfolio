d891706e9a8e30d489627e5ffd55fb30eb14ec4a
Lauren <laurenmiles.dev@gmail.com>
2023-09-15 10:09:24 -0400

    Added window constants to hold heights

---
+++ src/constants/window.ts
@@ -0,0 +1,8 @@
+// These are hardcoded header heights
+// Cannot get height of hidden windows without possible performance issues
+// These values will need updated if there are changes to the headers
+export default class WindowConstants {
+  static readonly WINDOW_HEADER_HEIGHT: number = 32;
+
+  static readonly IE_WINDOW_HEADER_HEIGHT: number = 128;
+}
