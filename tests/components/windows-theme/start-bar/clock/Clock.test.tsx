c5b34007644adc58487fb5e9c44e353d792c6780
Lauren <laurenmiles.dev@gmail.com>
2023-09-19 19:19:18 -0400

    Updated Clock component and tests

--- tests/components/windows-theme/start-bar/clock/Clock.test.tsx
+++ tests/components/windows-theme/start-bar/clock/Clock.test.tsx
@@ -1,26 +1,26 @@
 import { render } from '@testing-library/react';
 import Clock, {
-  id,
+  clockId,
   getCurrentTimeString,
 } from '../../../../../src/components/windows-theme/start-bar/clock/Clock';
 
-describe('Clock component', () => {
-  const currentTime = new Date(2023, 7, 30, 16, 4);
-  const expectedTimeString = '4:04 PM';
+const currentTime = new Date(2023, 7, 30, 16, 4);
+const expectedTimeString = '4:04 PM';
 
+describe('getCurrentTimeString', () => {
+  it('formats time into string', () => {
+    const response = getCurrentTimeString(currentTime);
+
+    expect(response).toEqual(expectedTimeString);
+  });
+});
+
+describe('Clock component', () => {
   it('renders current time', () => {
     render(<Clock currentTime={currentTime} />);
 
-    const element = document.getElementById(id) as HTMLDivElement;
+    const element = document.getElementById(clockId) as HTMLDivElement;
 
     expect(element).toHaveTextContent(expectedTimeString);
-  });
-
-  describe('getCurrentTimeString', () => {
-    it('formats time into string', () => {
-      const response = getCurrentTimeString(currentTime);
-
-      expect(response).toEqual(expectedTimeString);
-    });
   });
 });
