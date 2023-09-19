d08823727aec8d090fed9ce04d253f130b9bd0b1
Lauren <laurenmiles.dev@gmail.com>
2023-09-19 10:34:06 -0400

    Updated Footer component

--- src/components/portfolio/footer/Footer.tsx
+++ src/components/portfolio/footer/Footer.tsx
@@ -1,33 +1,25 @@
 import { Dispatch, SetStateAction } from 'react';
-import WindowsStartBar from '../../windows-theme/start-bar/StartBar';
-import WindowsThemeButton from '../../windows-theme/windows-theme-button/WindowsThemeButton';
+import WindowsThemeButton from './windows-theme-button/WindowsThemeButton';
 import Contacts from './contacts/Contacts';
 import ThemeSwitch from './theme-switch/ThemeSwitch';
 import ContactModel from '../../../models/contact/contact';
-import MenuItemModel from '../../../models/component-helpers/menu-item';
 
 import styles from './Footer.module.css';
 
 type Props = {
-  menuItems: MenuItemModel[];
   contacts: ContactModel[];
   useDarkTheme: boolean;
   setUseDarkTheme: Dispatch<SetStateAction<boolean>>;
-  useWindowsTheme: boolean;
   setTheme: Dispatch<SetStateAction<string>>;
 };
 
 export default function Footer({
-  menuItems,
   contacts,
   useDarkTheme,
   setUseDarkTheme,
-  useWindowsTheme,
   setTheme,
 }: Props): JSX.Element {
-  return useWindowsTheme ? (
-    <WindowsStartBar menuItems={menuItems} useDarkTheme={useDarkTheme} setTheme={setTheme} />
-  ) : (
+  return (
     <div className={`${styles.container} col-md-12`}>
       <WindowsThemeButton setTheme={setTheme} useDarkTheme={useDarkTheme} />
 
