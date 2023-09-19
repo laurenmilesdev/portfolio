77c13ef01fa4b774fac0061c694bc4d417b607b9
Lauren <laurenmiles.dev@gmail.com>
2023-09-19 10:34:18 -0400

    Created Layout component for windows theme

---
+++ src/components/windows-theme/layout/Layout.tsx
@@ -0,0 +1,26 @@
+import { Dispatch, SetStateAction } from 'react';
+import Desktop from '../desktop/Desktop';
+import StartBar from '../start-bar/StartBar';
+import DesktopItemModel from '../../../models/component-helpers/desktop-item';
+import MenuItemModel from '../../../models/component-helpers/menu-item';
+
+type Props = {
+  desktopItems: DesktopItemModel[];
+  menuItems: MenuItemModel[];
+  useDarkTheme: boolean;
+  setTheme: Dispatch<SetStateAction<string>>;
+};
+
+export default function Layout({
+  desktopItems,
+  menuItems,
+  useDarkTheme,
+  setTheme,
+}: Props): JSX.Element {
+  return (
+    <>
+      <Desktop desktopItems={desktopItems} />
+      <StartBar menuItems={menuItems} useDarkTheme={useDarkTheme} setTheme={setTheme} />
+    </>
+  );
+}
