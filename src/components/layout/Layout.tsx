8ce215c479d05f5d9fa6610cc78ebdc01ca9ae9f
Lauren <laurenmiles.dev@gmail.com>
2023-09-19 09:26:00 -0400

    Added Fade to pages

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,4 @@
 import { Container } from '@mui/material';
-import Fade from '@mui/material/Fade';
 import Navigation from '../navigation/Navigation';
 
 import styles from './Layout.module.css';
@@ -34,21 +33,19 @@ export default function Layout({
         useWindowsTheme={useWindowsTheme}
       />
 
-      <Fade in={true}>
-        <Container>
-          <div className={styles.container}>
-            {title && (
-              <div className={`${styles.title} col-md-12 pb-3`} id={titleDivId}>
-                <h1>{title}</h1>
-              </div>
-            )}
-
-            <div className="col-md-12" id={contentDivId}>
-              {children}
+      <Container>
+        <div className={styles.container}>
+          {title && (
+            <div className={`${styles.title} col-md-12 pb-3`} id={titleDivId}>
+              <h1>{title}</h1>
             </div>
+          )}
+
+          <div className="col-md-12" id={contentDivId}>
+            {children}
           </div>
-        </Container>
-      </Fade>
+        </div>
+      </Container>
     </>
   );
 }
--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
+import Fade from '@mui/material/Fade';
 
+import React from 'react';
 import img from '../../../../public/img/home/headshot.png';
 import styles from './Home.module.css';
 
@@ -16,29 +18,31 @@ export const altText = 'Image of Lauren within text that says Lauren Miles';
 
 export default function Home({ subtitle, description, useWindowsTheme }: Props): JSX.Element {
   return (
-    <div className={`${styles.container} col-md-12`}>
-      <div
-        className={`${
-          useWindowsTheme ? styles['windows-theme-subtitle'] : styles.subtitle
-        } col-md-12`}
-        id={subtitleDivId}
-      >
-        {subtitle}
-      </div>
+    <Fade in={true}>
+      <div className={`${styles.container} col-md-12`}>
+        <div
+          className={`${
+            useWindowsTheme ? styles['windows-theme-subtitle'] : styles.subtitle
+          } col-md-12`}
+          id={subtitleDivId}
+        >
+          {subtitle}
+        </div>
 
-      <div className={`${styles.description} col-md-12`} id={descriptionDivId}>
-        {description}
-      </div>
+        <div className={`${styles.description} col-md-12`} id={descriptionDivId}>
+          {description}
+        </div>
 
-      <div className="col-md-12">
-        <Image
-          src={img}
-          alt={altText}
-          className={useWindowsTheme ? styles['windows-theme-logo'] : styles.logo}
-          id={imageId}
-          priority
-        />
+        <div className="col-md-12">
+          <Image
+            src={img}
+            alt={altText}
+            className={useWindowsTheme ? styles['windows-theme-logo'] : styles.logo}
+            id={imageId}
+            priority
+          />
+        </div>
       </div>
-    </div>
+    </Fade>
   );
 }
