913ad028101a0772a2e2c7b9befaecf0f078633f
Lauren <laurenmiles.dev@gmail.com>
2023-10-09 10:03:56 -0400

    Updated HelpWindowsTabs component

--- src/components/windows-theme/help-window/content/tabs/HelpWindowTabs.tsx
+++ src/components/windows-theme/help-window/content/tabs/HelpWindowTabs.tsx
@@ -1,3 +1,4 @@
+import { Dispatch, SetStateAction } from 'react';
 import Image from 'next/image';
 import { Tab, Tabs } from '@mui/material';
 
@@ -6,10 +7,10 @@ import styles from './HelpWindowTabs.module.css';
 type Props = {
   helpTabTitles: string[];
   tabValue: number;
-  handleChange: (event: React.SyntheticEvent, newValue: number) => void;
+  setHelpTabValue: Dispatch<SetStateAction<number>>;
 };
 
-export default function HelpWindowTabs({ helpTabTitles, tabValue, handleChange }: Props) {
+export default function HelpWindowTabs({ helpTabTitles, tabValue, setHelpTabValue }: Props) {
   const helpImageLogo = (
     <Image
       src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgBAMAAACBVGfHAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAGFBMVEUAAACAAIAAAACAgICAgAD//wDAwMD///9lJw0+AAAAAXRSTlMAQObYZgAAAAFiS0dEBxZhiOsAAAAHdElNRQfiBhgXChR06RP7AAAAvUlEQVQoz3XRUQ6DIAyAYeYNKOwd8QTLEn1eMg+wxF7AB7zBvP7aUlBM5hufP5QEY/58N7iAs7ENHrYVZ20jHDQiwUk4mPF1CAV3RKwNB7i88UMAGnj6LRDKCE9GAKGOmOWMIZQ70JqmwBg08Ljw1CGFcknPaxgJIMoOXlPA8GSRkXZMKRpILHM+Ia1gYPtKIyeknrZsu0oOGLJoYKybsmhgul5Fg0M0qLKXoEoNihyByto8Jsk5ELm+dqfBDwcgN9nfkQsOAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDE4LTA2LTI0VDIzOjEwOjIwLTA0OjAw2N+qCAAAACV0RVh0ZGF0ZTptb2RpZnkAMjAxOC0wNi0yNFQyMzoxMDoyMC0wNDowMKmCErQAAAAASUVORK5CYII="
@@ -23,7 +24,7 @@ export default function HelpWindowTabs({ helpTabTitles, tabValue, handleChange }
   return (
     <Tabs
       value={tabValue}
-      onChange={handleChange}
+      onChange={(event, newValue) => setHelpTabValue(newValue as number)}
       className="help-tabs"
       orientation="vertical"
       centered
