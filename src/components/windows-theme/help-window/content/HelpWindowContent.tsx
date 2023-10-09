343ab946c9d123d91582389cb89f1f8fd23a01fa
Lauren <laurenmiles.dev@gmail.com>
2023-10-09 10:03:38 -0400

    Updated HelpWindowContent component

--- src/components/windows-theme/help-window/content/HelpWindowContent.tsx
+++ src/components/windows-theme/help-window/content/HelpWindowContent.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Dispatch, SetStateAction } from 'react';
 import HelpWindowTabs from './tabs/HelpWindowTabs';
 import HelpWindowTabsContent from './tabs-content/HelpWindowTabsContent';
 import PageModel from '../../../../models/component-helpers/page';
@@ -7,27 +7,25 @@ import styles from './HelpWindowContent.module.css';
 
 type Props = {
   helpTabs: PageModel[];
+  helpTabValue: number;
+  setHelpTabValue: Dispatch<SetStateAction<number>>;
 };
 
-export default function HelpWindowContent({ helpTabs }: Props) {
-  const [tabValue, setTabValue] = useState<number>(0);
-
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => setTabValue(newValue);
-
+export default function HelpWindowContent({ helpTabs, helpTabValue, setHelpTabValue }: Props) {
   return (
     <div className="col-md-12 d-flex justify-content-center">
       <div className={`${styles['help-tabs-container']} col-md-5`}>
         <HelpWindowTabs
           helpTabTitles={helpTabs.map((tab) => tab.title)}
-          tabValue={tabValue}
-          handleChange={handleChange}
+          tabValue={helpTabValue}
+          setHelpTabValue={setHelpTabValue}
         />
       </div>
 
       <div className="col-md-1"></div>
 
       <div className={`${styles['help-tabs-content-container']} help-tabs col-md-5`}>
-        <HelpWindowTabsContent helpTabs={helpTabs} tabValue={tabValue} />
+        <HelpWindowTabsContent helpTabs={helpTabs} tabValue={helpTabValue} />
       </div>
     </div>
   );
