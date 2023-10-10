2227f3110285478174ebf9c21407bcad427b9d5c
Lauren <laurenmiles.dev@gmail.com>
2023-10-10 12:46:04 -0400

    Updated HelpWindow component

--- src/components/windows-theme/help-window/HelpWindow.tsx
+++ src/components/windows-theme/help-window/HelpWindow.tsx
@@ -1,6 +1,6 @@
 import { Dispatch, SetStateAction } from 'react';
-import HelpWindowHeader from './header/HelpWindowHeader';
-import HelpWindowContent from './content/HelpWindowContent';
+import HelpWindowHeader from './help-window-header/HelpWindowHeader';
+import HelpWindowContent from './help-window-content/HelpWindowContent';
 import PageModel from '../../../models/component-helpers/page';
 
 import styles from './HelpWindow.module.css';
@@ -13,17 +13,45 @@ type Props = {
 export default function HelpWindow({ helpTabValue, setHelpTabValue }: Props) {
   const helpTabs: PageModel[] = [
     new PageModel(
-      'Why is my copy of Windows feature limited?',
+      'Why are the Windows features limited?',
       tabContent(
-        'Why is my copy of Windows feature limited?',
-        'Since this version of Windows was built entirely with CSS, you may notice some features are missing. This is because of the limitations of CSS.'
+        'Why are the Windows features limited?',
+        <>
+          Since most of this Windows theme was created using CSS, unfortunately with that comes
+          limitations. CSS is simply the styling applied to a website or application, it does not
+          include the user interactivity. Since I used JavaScript for some of the functionality,
+          enhancements can be made in the future to improve the UI experience.
+        </>
       )
     ),
     new PageModel(
-      "Why can't I move windows?",
+      'Why create a Windows theme for my portfolio?',
       tabContent(
-        "Why can't I move windows?",
-        'Although CSS lets you do quite a lot of things, unfortunately drag and drop is not one of them.'
+        'Why create a Windows theme for my portfolio?',
+        <>
+          Nostalgia. Technology has rapidly advanced in the past 50 years and I think sometimes we
+          miss simpler times. This theme reminds me of when I would have to logoff the internet to
+          make a phone call on the landline.
+        </>
+      )
+    ),
+    new PageModel(
+      'How did I create this theme?',
+      tabContent(
+        'How did I create this theme?',
+        <>
+          As a typical developer, I wanted a jumping off point and some basic CSS already laid out
+          so it would take less time to create the foundation. I stumbled upon a very helpful
+          project{' '}
+          <a href="https://codepen.io/smpnjn/pen/ExLbvdJ?editors=1100" target="_blank">
+            CSS Only Windows 98
+          </a>{' '}
+          (got to give credit where credit is due). This project helped me layout the general
+          styling used for an old Windows OS. I took some of the CSS styling straight from that
+          project, but updated a lot of it to be more concise while also adding in responsiveness.
+          Instead of using CSS for any functionality, I created JavaScript code for it. The
+          functionality within the CSS Only Windows 98 project was more limited than I wanted.
+        </>
       )
     ),
   ];
@@ -41,7 +69,7 @@ export default function HelpWindow({ helpTabValue, setHelpTabValue }: Props) {
   );
 }
 
-export function tabContent(title: string, description: string): JSX.Element {
+export function tabContent(title: string, description: JSX.Element): JSX.Element {
   return (
     <>
       <h2>{title}</h2>
