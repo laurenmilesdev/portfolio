84bf6d493ddcd0eefb17c14c7772f61228651604
Lauren <laurenmiles.dev@gmail.com>
2023-11-03 12:32:00 -0400

    Updated Home

--- src/components/portfolio/pages/home/Home.tsx
+++ src/components/portfolio/pages/home/Home.tsx
@@ -1,7 +1,5 @@
 type Props = {
-  subtitle: string;
   description: string;
-  useWindowsTheme: boolean;
 };
 
 export const subtitleDivId = 'home-subtitle';
@@ -9,16 +7,12 @@ export const descriptionDivId = 'home-description';
 export const imageId = 'logo';
 export const altText = 'Image of Lauren within text that says Lauren Miles';
 
-export default function Home({ subtitle, description }: Props): JSX.Element {
+export default function Home({ description }: Props): JSX.Element {
   return (
-    <div className={`col-md-12`}>
-      <div className={`col-md-12`} id={subtitleDivId}>
-        <h1>{subtitle}</h1>
-      </div>
-
-      <div className={`col-md-12`} id={descriptionDivId}>
-        {description}
-      </div>
-    </div>
+    <div
+      className={`col-md-12`}
+      id={descriptionDivId}
+      dangerouslySetInnerHTML={{ __html: description }}
+    ></div>
   );
 }
