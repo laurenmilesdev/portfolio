9333435a7e539cbb820354fe917f2b6a80a768d2
Lauren <laurenmiles.dev@gmail.com>
2023-11-04 09:23:26 -0400

    Removed unnecessary consts

--- src/components/portfolio/pages/home/Home.tsx
+++ src/components/portfolio/pages/home/Home.tsx
@@ -2,10 +2,7 @@ type Props = {
   description: string;
 };
 
-export const subtitleDivId = 'home-subtitle';
 export const descriptionDivId = 'home-description';
-export const imageId = 'logo';
-export const altText = 'Image of Lauren within text that says Lauren Miles';
 
 export default function Home({ description }: Props): JSX.Element {
   return (
