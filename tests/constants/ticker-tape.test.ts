c0428130179b349f82679afba1f2ef09232b0d5c
Lauren <laurenmiles.dev@gmail.com>
2023-11-14 16:47:16 -0500

    Created unit test for ticker-tape const

---
+++ tests/constants/ticker-tape.test.ts
@@ -0,0 +1,16 @@
+import TickerTapeConstants from '../../src/constants/ticker-tape';
+
+describe('TickerTapeConstants', () => {
+  it('returns OPEN_TO_WORK constant', () => {
+    const response = TickerTapeConstants.OPEN_TO_WORK;
+
+    expect(response).toEqual([
+      'OPEN TO WORK',
+      'OPEN TO WORK',
+      'OPEN TO WORK',
+      'OPEN TO WORK',
+      'OPEN TO WORK',
+      'OPEN TO WORK',
+    ]);
+  });
+});
