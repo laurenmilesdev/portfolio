4b76ff6edd5c995d26a478b87219ac44040a11f5
Lauren <laurenmiles.dev@gmail.com>
2023-12-28 16:12:26 -0500

    Updated fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link
-          href="https://fonts.googleapis.com/css2?family=Lacquer&family=Rock+3D&family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&family=Monofett&display=swap"
           rel="stylesheet"
         ></link>
       </Head>
