59c05d0ae9b2a67a623b95a02a0a2247712bbd0f
Lauren <laurenmiles.dev@gmail.com>
2023-12-28 17:21:00 -0500

    Fixed test

--- tests/mocks/data-mock.tsx
+++ tests/mocks/data-mock.tsx
@@ -7,7 +7,7 @@ import DesktopItemModel from '../../src/models/component-helpers/desktop-item';
 export const contacts = [
   new ContactModel('GitHub', 'Contact description.', 'Contact value', 'github.com'),
   new ContactModel('LinkedIn', 'Contact description 2.', 'Contact value 2', 'linkedin.com'),
-  new ContactModel('Resume', 'Contact description 3.', 'Contact value 3'),
+  new ContactModel('Email', 'Contact description 3.', 'Contact value 3'),
   new ContactModel('None', 'Contact description 4.', 'Contact value 4'),
 ];
 
